09eed4bcdd727447898a2ba87a15af5ecac9a3fe
vklym <klym2612@gmail.com>
2019-06-23 16:38:19 +0300

    test

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 export default function Error() {
     return (
         <Hero>
-            <Banner title='404' subtitle='page not found'>
+            <Banner title='404' subtitle='sorry, page not found'>
                 <Link to='/' className='btn-primary'>
                     return home
                 </Link>
